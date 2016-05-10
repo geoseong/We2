1d3c5200613b4bb24ef083b50cd395771bf77784
geoseong <imf4@naver.com>
2016-05-11 01:07:21 +0900

    Revert "Revert "160510 - AOP테스트성공""
    This reverts commit 32692259e849e936d80e903d280ebc05e3b3d8b9.

---
+++ We2/src/main/java/interceptor/AOP_Aspect.java
@@ -0,0 +1,8 @@
+package interceptor;
+
+public class AOP_Aspect {
+
+	public void letsgoaop(){
+		System.out.println("AOP:Before ¿¹Á¦");
+	}
+}
---
+++ We2/src/main/java/interceptor/AOP_Test.java
@@ -0,0 +1,14 @@
+package interceptor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*@Controller*/
+public class AOP_Test {
+	/*
+	@RequestMapping(value="/aop")
+	public void aopTest(){
+		System.out.println("AOP_test ¿‘¥œ¥Ÿ.\n---------------");
+	}*/
+
+}
